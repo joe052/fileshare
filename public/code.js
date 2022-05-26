881d46005602968e13d356c46ea71c11638c57ca
Joe Karanja <joe.karanja@vault.africa.ai>
2022-05-26 16:39:10 +0300

    message

---
+++ public/code.js
@@ -0,0 +1,20 @@
+(function(){
+    let receiverID;
+    const socket = io();
+
+    function generateID(){
+        return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
+    }
+
+    document.querySelector('#sender-start-con-btn').addEventListener('click',()=>{
+        let joinID = generateID();
+        document.querySelector('#join-id').innerHTML = `
+        <b>Room ID</b>
+        <span>${joinID}</span>
+        `;
+        socket.emit("sender-join",{
+            uid: joinID
+        });
+    });
+
+})();
